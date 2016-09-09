5b5b452f97e9274be81f729ec259cb528051e63d
Zabriskie <patrickzabriskie@MacBook-Pro-4.attlocal.net>
2016-09-09 14:35:25 -0400

    Upload
    Upload

---
+++ Resources/ConvertTemperature.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace MeasureConvert
+{
+	public class ConvertTemperature
+	{
+		public static String[] labels = new String[3] { "Fahrenheit", "Celsius", "Kelvin"};
+
+		double RoundToSignificantDigits(double d, int digits)
+		{
+			double EPSILON = 0.00000001;
+			if (Math.Abs(d) < EPSILON)
+			{
+				return 0;
+			}
+
+			double scale = Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(d))) + 1);
+			return scale * Math.Round(d / scale, digits);
+		}
+
+		public double Fahrenheit_Celsius(double number, int sign)
+		{
+			if (sign > 0) { return RoundToSignificantDigits(number = 5.0 / 9.0 * (number - 32.0),4);}
+			else { return RoundToSignificantDigits(number = 9.0 / 5.0 * number + 32.0, 4); }
+		}
+
+		public double Celsius_Kelvin(double number, int sign)
+		{
+			if (sign > 0) { return RoundToSignificantDigits(number + 273.15,4); }
+			else { return RoundToSignificantDigits(number -273.15,4); }
+		}
+
+		public double numbers_functions(int assign, double value, int sign)
+		{
+			double answer = -1.0;
+			switch (assign)
+			{
+				case 0:
+					answer = Fahrenheit_Celsius(value, sign);
+					break;
+				case 1:
+					answer = Celsius_Kelvin(value, sign);
+					break;
+			}
+			return answer;
+		}
+
+
+		int first_index;
+		int second_index;
+		int index_difference;
+		int index_sign;
+		double value_1;
+		public ConvertTemperature(string firstunit, string secondunit, string value)
+		{
+			first_index = Array.IndexOf(labels, firstunit);
+			second_index = Array.IndexOf(labels, secondunit);
+			index_difference = second_index - first_index;
+			index_sign = Math.Sign(index_difference);
+			value_1 = Double.Parse(value);
+		}
+
+		public string return_value()
+		{
+			if (index_sign > 0)
+			{
+				for (int i = this.first_index; i < this.second_index; i++)
+				{
+					value_1 = numbers_functions(i, value_1, index_sign);
+				}
+			}
+			else
+			{
+				for (int i = this.first_index - 1; i >= this.second_index; i = i - 1)
+				{
+					
+					value_1 = numbers_functions(i, value_1, index_sign);
+				}
+
+			}
+			return value_1.ToString();
+		}
+
+
+	}
+}
+
---
+++ Resources/mipmap-mdpi/MainActivity.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MeasureConvert
+{
+	[Activity(Label = "MeasureConvert", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		//int count = 1;
+		private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+		{
+			//Spinner spinner = (Spinner)sender;
+
+			//string toast = string.Format("The planet is {0}", spinner.GetItemAtPosition(e.Position));
+			//Toast.MakeText(this, toast, ToastLength.Long).Show();
+		}
+
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+
+			Spinner spinner = FindViewById<Spinner>(Resource.Id.MeasurementType);
+			Spinner startunits = FindViewById<Spinner>(Resource.Id.StartUnits);
+			Spinner targetunits = FindViewById<Spinner>(Resource.Id.TargetUnits);
+
+			spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+			var adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.Measurements, Android.Resource.Layout.SimpleSpinnerItem);
+			adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+			spinner.Adapter = adapter;
+
+			Button enterbutton = FindViewById<Button>(Resource.Id.Enter);
+			TextView selectstart = FindViewById<TextView>(Resource.Id.SelectStart);
+			TextView title = FindViewById<TextView>(Resource.Id.Title);
+			TextView selecttarget = FindViewById<TextView>(Resource.Id.SelectTarget);
+			EditText input = FindViewById<EditText>(Resource.Id.Pounds);
+			TextView output = FindViewById<TextView>(Resource.Id.Answer);
+			Button convertbutton = FindViewById<Button>(Resource.Id.Convert);
+			TextView enternumber = FindViewById<TextView>(Resource.Id.EnterNumber);
+			Button resetbutton = FindViewById<Button>(Resource.Id.Reset);
+
+			startunits.Visibility = ViewStates.Invisible;
+			targetunits.Visibility = ViewStates.Invisible;
+			selectstart.Visibility = ViewStates.Invisible;
+			selecttarget.Visibility = ViewStates.Invisible;
+			input.Visibility = ViewStates.Invisible;
+			output.Visibility = ViewStates.Invisible;
+			convertbutton.Visibility = ViewStates.Invisible;
+			enternumber.Visibility = ViewStates.Invisible;
+			resetbutton.Visibility = ViewStates.Gone;
+
+
+			enterbutton.Click += (object sender, EventArgs e) =>
+			{
+				title.Visibility = ViewStates.Gone;
+				startunits.Visibility = ViewStates.Visible;
+				targetunits.Visibility = ViewStates.Visible;
+				selectstart.Visibility = ViewStates.Visible;
+				selecttarget.Visibility = ViewStates.Visible;
+				input.Visibility = ViewStates.Visible;
+				output.Visibility = ViewStates.Visible;
+				convertbutton.Visibility = ViewStates.Visible;
+				enternumber.Visibility = ViewStates.Visible;
+				enterbutton.Visibility = ViewStates.Gone;
+				spinner.Visibility = ViewStates.Gone;
+				resetbutton.Visibility = ViewStates.Visible;
+
+				selectstart.Text = "Select starting units";
+				selecttarget.Text = "Select Ending units";
+
+				if (spinner.SelectedItem.ToString() == "Distance")
+				{
+					startunits.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+					var adapter1 = ArrayAdapter.CreateFromResource(this, Resource.Array.Distance, Android.Resource.Layout.SimpleSpinnerItem);
+					adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+					startunits.Adapter = adapter1;
+
+					targetunits.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+					var adapter2 = ArrayAdapter.CreateFromResource(this, Resource.Array.Distance, Android.Resource.Layout.SimpleSpinnerItem);
+					adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+					targetunits.Adapter = adapter2;
+				}
+
+				else if (spinner.SelectedItem.ToString() == "Weight")
+				{
+					startunits.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+					var adapter1 = ArrayAdapter.CreateFromResource(this, Resource.Array.Weight, Android.Resource.Layout.SimpleSpinnerItem);
+					adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+					startunits.Adapter = adapter1;
+
+					targetunits.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+					var adapter2 = ArrayAdapter.CreateFromResource(this, Resource.Array.Weight, Android.Resource.Layout.SimpleSpinnerItem);
+					adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+					targetunits.Adapter = adapter2;
+				}
+
+				else if (spinner.SelectedItem.ToString() == "Volume")
+				{
+					startunits.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+					var adapter1 = ArrayAdapter.CreateFromResource(this, Resource.Array.Volume, Android.Resource.Layout.SimpleSpinnerItem);
+					adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+					startunits.Adapter = adapter1;
+
+					targetunits.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+					var adapter2 = ArrayAdapter.CreateFromResource(this, Resource.Array.Volume, Android.Resource.Layout.SimpleSpinnerItem);
+					adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+					targetunits.Adapter = adapter2;
+				}
+				else
+				{
+					startunits.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+					var adapter1 = ArrayAdapter.CreateFromResource(this, Resource.Array.Temperature, Android.Resource.Layout.SimpleSpinnerItem);
+					adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+					startunits.Adapter = adapter1;
+
+					targetunits.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+					var adapter2 = ArrayAdapter.CreateFromResource(this, Resource.Array.Temperature, Android.Resource.Layout.SimpleSpinnerItem);
+					adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+					targetunits.Adapter = adapter2;
+				}
+
+			};
+			convertbutton.Click += (object sender, EventArgs e) =>
+			{
+				double n;
+				string pounds = "";
+				pounds = input.Text;
+
+				if (!Double.TryParse(pounds.ToString(), out n))
+				{
+					output.Text = "Must Give A Number";
+				}
+
+				else if (!(Double.Parse(pounds.ToString()) >= 0) && !(spinner.SelectedItem.ToString() == "Temperature"))
+				{
+					output.Text = "Number Must Be Non-Negative";
+				}
+
+				else
+				{
+					ClassAssign classassign = new ClassAssign();
+					output.Text = classassign.call_class(spinner.SelectedItem.ToString(),
+														  startunits.SelectedItem.ToString(),
+														  targetunits.SelectedItem.ToString(),
+														  pounds.ToString()) + " "
+															+ targetunits.SelectedItem;
+
+
+				}
+			
+			};
+			resetbutton.Click += (object sender, EventArgs e) =>
+			{
+				title.Visibility = ViewStates.Visible;
+				startunits.Visibility = ViewStates.Invisible;
+				targetunits.Visibility = ViewStates.Invisible;
+				selectstart.Visibility = ViewStates.Invisible;
+				selecttarget.Visibility = ViewStates.Invisible;
+				input.Visibility = ViewStates.Invisible;
+				output.Visibility = ViewStates.Gone;
+				convertbutton.Visibility = ViewStates.Invisible;
+				spinner.Visibility = ViewStates.Visible;
+				enternumber.Visibility = ViewStates.Invisible;
+				enterbutton.Visibility = ViewStates.Visible;
+				resetbutton.Visibility = ViewStates.Gone;
+				output.Text = "";
+			};
+		
+		
+		}
+	}
+}
+
+
