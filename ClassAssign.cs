32389d45e9f61d8516d046d20951ac1a6efd7b8e
Zabriskie <patrickzabriskie@MacBook-Pro-4.attlocal.net>
2016-09-09 14:33:39 -0400

    Upload
    Upload

---
+++ ClassAssign.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace MeasureConvert
+{
+	public class ClassAssign
+	{
+		public string call_class(string measurement, string startingunit, string endingunit, string value)
+		{
+			if (startingunit.Equals(endingunit)) { return value; }
+
+			if (measurement.Equals("Distance")) 
+			{ 
+				String[] labels = new String[6] { "Miles", "Feet", "Inches", "Centimeters", "Meters", "Kilometers" };
+				double[] dividends = { 5280.0, 12.0, 2.54, 1.0 / 100.0, 1.0 / 1000.0 };
+				ConvertUnits convertdistance = new ConvertUnits(labels, dividends, startingunit, endingunit, value);
+				return convertdistance.return_value();
+			}
+			else if (measurement.Equals("Weight")) 
+			{ 
+				String[] labels = new String[7] { "Tons", "Slugs", "Pounds", "Ounces", "Grams", "Kilograms", "Metric Tons" };
+				double[] dividends = new double[6] { 62.1619, 32.174, 16.0, 28.3495, 1.0 / 1000.0, 1.0 / 1000.0 };
+				ConvertUnits convertweight=new ConvertUnits(labels, dividends, startingunit, endingunit, value);
+				return convertweight.return_value();
+			
+			}
+
+			else if (measurement.Equals("Volume"))
+			{
+				String[] labels = new String[11] { "Gallons", "Quarts", "Pints", "Cups", "Fluid Ounces", "Milliliters", "Liters", "Cubic Meters", "Cubic Inches", "Cubic Feet", "Cubic Yards" };
+				double[] dividends = new double[10] { 4.0, 2.0, 2.0, 8.0, 29.5735, 0.001, 0.001, 61023.7, 0.000578704, 0.037037 };
+				ConvertUnits convertvolume = new ConvertUnits(labels, dividends, startingunit, endingunit, value);
+				return convertvolume.return_value();
+
+			}
+
+			else 
+			{
+				ConvertTemperature converttemp = new ConvertTemperature(startingunit, endingunit, value);
+				return converttemp.return_value();
+			}
+		
+		}
+
+		public ClassAssign()
+		{
+		}
+	}
+}
+
---
+++ ConvertUnits.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace MeasureConvert
+{
+	public class ConvertUnits
+	{
+		private String[] labels;
+		private double[] dividends;
+		private int first_index;
+		private int second_index;
+		private int index_difference;
+		private int index_sign;
+		private double value_1;
+
+
+		public ConvertUnits(String[] Labels, double[] numbers, string firstunit, string secondunit, string value)
+		{
+			this.labels = Labels;
+			this.dividends = numbers;
+			this.first_index = Array.IndexOf(labels, firstunit);
+			this.second_index = Array.IndexOf(labels, secondunit);
+			this.index_difference = second_index - first_index;
+			this.index_sign = Math.Sign(index_difference);
+			this.value_1 = Double.Parse(value);
+		}
+	
+		public double multiply_divide(double number2, double number1, int sign)
+		{
+			if (sign > 0) { return RoundToSignificantDigits(number1 * number2, 4); }
+			else { return RoundToSignificantDigits(number1 / number2, 4); }
+		}
+
+		double RoundToSignificantDigits(double d, int digits)
+		{
+			double EPSILON = 0.00000001;
+			if (Math.Abs(d) < EPSILON)
+			{
+				return 0;
+			}
+
+			double scale = Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(d))) + 1);
+			return scale * Math.Round(d / scale, digits);
+		}
+		public string return_value()
+		{
+			if (index_sign > 0)
+			{
+				for (int i = first_index; i < second_index; i++)
+				{
+					value_1 = multiply_divide(dividends[i], value_1, index_sign);
+					//value_1 = numbers_functions(i, value_1, index_sign);
+				}
+			}
+			else
+			{
+				for (int i = first_index - 1; i >= second_index; i = i - 1)
+				{
+					value_1 = multiply_divide(dividends[i], value_1, index_sign);
+					//value_1 = numbers_functions(i, value_1, index_sign);
+				}
+
+			}
+			return value_1.ToString();
+		}
+	
+	
+	}
+}
+
